d868945e5fdad0775b1b9f563080f1244165e5a9
peterAnami <peter.andake@student.moringaschool.com>
2021-10-30 12:11:32 +0300

    settings modifications

--- neighbourhood/settings.py
+++ neighbourhood/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'neighbor_app',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -117,4 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+# STATIC_URL = '/static/'
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
