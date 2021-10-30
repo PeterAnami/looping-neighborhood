2a8c7c4ad39c2073699d16d3a2c4e349bd3b3ec3
peterAnami <peter.andake@student.moringaschool.com>
2021-10-31 00:33:26 +0300

    test file creation

--- neighbor_app/tests.py
+++ neighbor_app/tests.py
@@ -4,7 +4,6 @@ from __future__ import unicode_literals
 from django.test import TestCase
 
 # Create your tests here.
-# Create your tests here.
 from neighbor_app.views import neighborhood
 from django.test import TestCase
 from .models import Business,Profile,NeighborHood,Post
